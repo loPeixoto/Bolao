cd9c62f3d690d446bfa547dcd468d7ab713a9c8c
Lorenzo Peixoto Almeida <lo.peixoto@gmail.com>
2023-04-24 18:42:47 +0000

    Criando todos os atributos

--- projeto_Bolao/Aposta.java
+++ projeto_Bolao/Aposta.java
@@ -1,5 +1,10 @@
 package projeto_Bolao;
 
+import java.util.ArrayList;
+
 public class Aposta {
+    private ArrayList<Integer> numeros = new ArrayList<>();
+    private Jogador organizador;
+    private ArrayList<Jogador> jogadores = new ArrayList<>();
     
 }
--- projeto_Bolao/Bolao.java
+++ projeto_Bolao/Bolao.java
@@ -1,5 +1,9 @@
 package projeto_Bolao;
 
+import java.util.ArrayList;
+
 public class Bolao {
+    private ArrayList<Aposta> apostas = new ArrayList<>();
+    private ArrayList<Jogador> jogadores = new ArrayList<>();
     
 }
