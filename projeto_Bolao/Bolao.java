8c0d5f5ffce43d179b37071e419e212faa8b82a4
Lorenzo Peixoto Almeida <lo.peixoto@gmail.com>
2023-04-24 19:42:15 +0000

    Criando construtores e métodos da classe Pessoa

--- projeto_Bolao/Bolao.java
+++ projeto_Bolao/Bolao.java
@@ -2,8 +2,10 @@ package projeto_Bolao;
 
 import java.util.ArrayList;
 
-public class Bolao {
+public static void main(String[] args) {
     private ArrayList<Aposta> apostas = new ArrayList<>();
     private ArrayList<Jogador> jogadores = new ArrayList<>();
+
+
     
 }
--- projeto_Bolao/Pessoa.java
+++ projeto_Bolao/Pessoa.java
@@ -1,7 +1,37 @@
 package projeto_Bolao;
 
+import java.util.Scanner;
+
 public class Pessoa {
     protected String nome;
     protected String cpf;
+
+    public Pessoa() {
+        Scanner r = new Scanner(System.in);
+
+        r.nextLine();
+
+        System.out.println("Digite o nome:\n");
+        this.nome = r.nextLine();
+
+        r.nextLine();
+
+        System.out.println("Digite o CPF: \n");
+        this.cpf = r.nextLine();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void listarDados() {
+        System.out.println("*** Dados da Pessoa ***");
+        System.out.println("Nome: " + this.nome);
+        System.out.println("CPF: " + this.cpf);
+    }
     
 }
