02393ea99bf1b7f71a860cf812a672c61a4c7e3e
Mateus <mateuslannes100@gmail.com>
2023-04-25 19:36:41 +0000

    comentário teste

--- projeto_Bolao/Bolao.java
+++ projeto_Bolao/Bolao.java
@@ -3,7 +3,7 @@ package projeto_Bolao;
 import java.util.ArrayList;
 
 public static void main(String[] args) {
-    private ArrayList<Aposta> apostas;
+    private ArrayList<Aposta> apostas; // tem sómente 1 cartao
     private ArrayList<Jogador> jogadores;
 
 
