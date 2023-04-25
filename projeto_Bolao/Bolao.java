78d2d9bb0e019492b84d788e42d3915d3a4c2c06
Mateus <mateuslannes100@gmail.com>
2023-04-25 20:23:14 +0000

    Criando a classe Bolão

--- projeto_Bolao/Bolao.java
+++ projeto_Bolao/Bolao.java
@@ -2,10 +2,12 @@ package projeto_Bolao;
 
 import java.util.ArrayList;
 
-public static void main(String[] args) {
-    private ArrayList<Aposta> apostas; // tem sómente 1 cartao e minimo 2 jogadores
-    private ArrayList<Jogador> jogadores;
-
-
+public class Bolao{
+    public static void main(String[] args) {
+        private ArrayList<Aposta> apostas; // tem sómente 1 cartao e minimo 2 jogadores
+        private ArrayList<Jogador> jogadores;
     
+    
+        
+    }
 }
