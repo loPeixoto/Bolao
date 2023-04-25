727a70f82ba3beac6f21080d3b586f49fd0952f0
Lorenzo Peixoto Almeida <lo.peixoto@gmail.com>
2023-04-25 23:39:47 +0000

    inserir numeros

--- projeto_Bolao/Aposta.java
+++ projeto_Bolao/Aposta.java
@@ -1,6 +1,7 @@
 package projeto_Bolao;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Aposta {
     private ArrayList<Integer> numeros;
@@ -20,6 +21,33 @@ public class Aposta {
     }
     
     public void listarVencedores (double premio){
+
+    }
+
+    public void inserirNumeros() {
+        int cont, aux, q;
+        cont = 0;
+        Scanner ler = new Scanner(System.in);
+        System.out.println("*** Digite a quantidade de números da aposta ***");
+        q = ler.nextInt();
+
+        this.numeros = new ArrayList<Integer>();
+
+        while(numeros.size() <= q) {
+            aux = ler.nextInt();
+            if(aux <= 0 || aux > 60) {
+                System.out.println("Erro: digite um número válido de 1 a 60");
+            }else if(!this.numeros.contains(aux)) {
+                System.out.println("Erro: não pode repetir número");
+            }else {
+                this.numeros.add(aux);
+                cont ++;
+            }
+
+        }
+
+
+
 
     }
 }
