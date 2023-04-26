e9392749e0d6ca746b141900a4c3784d1a6bc2f8
Lorenzo Peixoto Almeida <lo.peixoto@gmail.com>
2023-04-26 00:12:11 +0000

    Construtor da classe Bolao

--- projeto_Bolao/Bolao.java
+++ projeto_Bolao/Bolao.java
@@ -7,5 +7,17 @@ public class Bolao{
     private ArrayList<Aposta> apostas; // tem sómente 1 cartao e minimo 2 jogadores
     private ArrayList<Jogador> jogadores;
 
+    public Bolao(ArrayList<Aposta> apostas, ArrayList<Jogador>jogadores) {
+        apostas = new ArrayList<Aposta>();
+        jogadores = new ArrayList<Jogador>();
+    }
+
+
+
+
+    public void cadastrarJogadores() {
+
+    }
+
     
 }
