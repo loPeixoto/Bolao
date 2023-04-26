62de6b81e8ae7649270040cf8c54dec3ac1f40d7
Mateus <mateuslannes100@gmail.com>
2023-04-26 18:59:41 +0000

    principal criado

--- projeto_Bolao/Principal.java
+++ projeto_Bolao/Principal.java
@@ -4,8 +4,23 @@ public class Principal {
 
     public static void main(String[] args) {
 
-    //aa
+    
+        Bolao bolao1 = new Bolao();
 
-        
+        int opcao = Menu.imprimeMenu();
+        while (opcao != 0 ){
+
+            if ( opcao == 1){
+                bolao1.cadastrarJogadores();
+            }
+            if ( opcao == 2){
+                bolao1.cadastrarAposta();
+            }
+            if ( opcao == 3){
+                // ainda nao foi feito
+            }
+            opcao = Menu.imprimeMenu();
+        }
+
     }
 }
